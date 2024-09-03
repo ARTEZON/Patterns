16fb6fb9fe8d0d01b64885c8e1f7df425300c20a
ARTEZON <artezon@yandex.ru>
2024-09-03 16:26:51 +0300

    Создан класс Student, конструктор, геттеры и сеттеры

---
+++ src/Student.kt
@@ -0,0 +1,28 @@
+class Student(
+    name: String, surname: String, patronym: String,
+    phone: String? = null, telegram: String? = null,
+    email: String? = null, git: String? = null
+) {
+    val id = 1; // TODO: ID autoincrement
+    var name = name
+        get() = field
+        set(value) { field = value }
+    var surname = surname
+        get() = field
+        set(value) { field = value }
+    var patronym = patronym
+        get() = field
+        set(value) { field = value }
+    var phone = phone
+        get() = field
+        set(value) { field = value }
+    var telegram = telegram
+        get() = field
+        set(value) { field = value }
+    var email = email
+        get() = field
+        set(value) { field = value }
+    var git = git
+        get() = field
+        set(value) { field = value }
+}
