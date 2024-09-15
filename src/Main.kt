247a75b94f080d6dcec2391a4228bb6ca9349d78
ARTEZON <artezon@yandex.ru>
2024-09-15 23:53:25 +0300

    [Lab1-Task5] Добавлен конструктор, принимающий hash map

--- src/Main.kt
+++ src/Main.kt
@@ -1,10 +1,43 @@
 fun main() {
     val students = mutableListOf(
-        Student(1, "Эзри", "Артём", "Александрович"),
-        Student(2, "Тестов", "Тест", "Тестович", "+79876543210", "@test123", "test@example.com", "https://github.com/test123"),
-        Student(3, "Иванов", "Василий", "Петрович", telegram="@vasya228", git="https://github.com/vasyacodes"),
-        Student(4, "Who", "Are", "You", git="https://gitlab.com/unknown"),
-        Student(5, "I", "Like", "Kotlin", email="kotlinisthebest@meow.com")
+        Student(mapOf(
+            "id" to 1,
+            "surname" to "Эзри",
+            "name" to "Артём",
+            "patronym" to "Александрович"
+        )),
+        Student(mapOf(
+            "id" to 2,
+            "surname" to "Тестов",
+            "name" to "Тест",
+            "patronym" to "Тестович",
+            "phone" to "+79876543210",
+            "telegram" to "@test123",
+            "email" to "test@example.com",
+            "git" to "https://github.com/test123"
+        )),
+        Student(mapOf(
+            "id" to 3,
+            "surname" to "Иванов",
+            "name" to "Василий",
+            "patronym" to "Петрович",
+            "telegram" to "@vasya228",
+            "git" to "https://github.com/vasyacodes"
+        )),
+        Student(mapOf(
+            "id" to 4,
+            "surname" to "Who",
+            "name" to "Are",
+            "patronym" to "You",
+            "git" to "https://gitlab.com/unknown"
+        )),
+        Student(mapOf(
+            "id" to 5,
+            "surname" to "I",
+            "name" to "Like",
+            "patronym" to "Kotlin",
+            "email" to "kotlinisthebest@meow.com"
+        )),
     )
 
     students.forEach { it.show() }
--- src/Student.kt
+++ src/Student.kt
@@ -44,6 +44,17 @@ class Student(
         this.git = git
     }
 
+    constructor(hashMap: Map<String, Any>) : this(
+        hashMap["id"]       as  Int,
+        hashMap["surname"]  as  String,
+        hashMap["name"]     as  String,
+        hashMap["patronym"] as  String,
+        hashMap["phone"]    as? String,
+        hashMap["telegram"] as? String,
+        hashMap["email"]    as? String,
+        hashMap["git"]      as? String,
+    )
+
     override fun toString(): String {
         var str = "[ID $id] $surname $name $patronym"
         if (phone != null) str += "\nНомер телефона: $phone"
