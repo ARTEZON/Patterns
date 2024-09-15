fbf3d550a41b5844740e997acec63511d2aee9bc
ARTEZON <artezon@yandex.ru>
2024-09-15 23:02:29 +0300

    Немного изменён код создания списка студентов

--- src/Main.kt
+++ src/Main.kt
@@ -1,10 +1,11 @@
 fun main() {
-    val students = mutableListOf<Student>()
-    students.add(Student(1, "Эзри", "Артём", "Александрович"))
-    students.add(Student(2, "Тестов", "Тест", "Тестович", "+79876543210", "@test123", "test@example.com", "https://github.com/test123"))
-    students.add(Student(3, "Иванов", "Василий", "Петрович", telegram="@vasya228", git="https://github.com/vasyacodes"))
-    students.add(Student(4, "Who", "Are", "You", git="https://gitlab.com/unknown"))
-    students.add(Student(5, "I", "Like", "Kotlin", email="kotlinisthebest@meow.com"))
+    val students = mutableListOf(
+        Student(1, "Эзри", "Артём", "Александрович"),
+        Student(2, "Тестов", "Тест", "Тестович", "+79876543210", "@test123", "test@example.com", "https://github.com/test123"),
+        Student(3, "Иванов", "Василий", "Петрович", telegram="@vasya228", git="https://github.com/vasyacodes"),
+        Student(4, "Who", "Are", "You", git="https://gitlab.com/unknown"),
+        Student(5, "I", "Like", "Kotlin", email="kotlinisthebest@meow.com")
+    )
 
     students.forEach { it.show() }
 }
