8068a4e9489fd866d907b1a26bd48db2c65b6e9a
ARTEZON <artezon@yandex.ru>
2024-10-08 15:53:10 +0300

    [Lab2-Part2-Task2] Реализованы методы для получения элемента, кол-ва строк и столбцов

--- src/DataTable.kt
+++ src/DataTable.kt
@@ -1,3 +1,7 @@
 class DataTable(private val array: Array<Array<Any>>) {
+    fun get(row: Int, col: Int) = array[row][col]
 
+    fun getRowCount() = array.size
+
+    fun getColCount() = if (array.isNotEmpty()) array[0].size else 0
 }
