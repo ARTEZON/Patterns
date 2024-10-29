1bd90fec23eb8047f75a05c7a68911535f0bc05c
ARTEZON <artezon@yandex.ru>
2024-10-29 19:09:29 +0300

    Добавлено определение драйвера SQLite в список зависимостей Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,8 @@ dependencies {
     testImplementation(kotlin("test"))
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
     implementation("com.charleskorn.kaml:kaml:0.61.0")
+    implementation("org.xerial:sqlite-jdbc:3.47.0.0")
+    implementation("org.slf4j:slf4j-reload4j:2.0.16")
 }
 
 tasks.test {
