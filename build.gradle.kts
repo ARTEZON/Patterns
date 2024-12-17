83cb03ea68c3c489182ebe1b32383f61f9a29648
ARTEZON <artezon@yandex.ru>
2024-12-17 11:26:09 +0300

    [Lab6] Реализован графический интерфейс

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,11 @@
 plugins {
     kotlin("jvm") version "2.0.21"
     kotlin("plugin.serialization") version "2.0.21"
+    java
+    id("org.openjfx.javafxplugin") version "0.1.0"
 }
 
-group = "org.example"
+group = "me.artezon.labs.patterns"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -16,6 +18,13 @@ dependencies {
     implementation("com.charleskorn.kaml:kaml:0.61.0")
     implementation("org.xerial:sqlite-jdbc:3.47.0.0")
     implementation("org.slf4j:slf4j-reload4j:2.0.16")
+    implementation("org.openjfx:javafx-controls:23.0.1")
+    implementation("org.openjfx:javafx-fxml:23.0.1")
+}
+
+javafx {
+    version = "23"
+    modules = listOf("javafx.controls", "javafx.fxml") // Add more modules as needed
 }
 
 tasks.test {
